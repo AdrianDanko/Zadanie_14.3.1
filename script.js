3ac16cdc3b1c231cc52c73b062ec66bc38795942
AdrianDanko <102218518+AdrianDanko@users.noreply.github.com>
2022-05-17 15:21:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+let zoznam = JSON.parse(localStorage.getItem("zoznam"));
+
+if (zoznam == null) {
+    zoznam = [];
+}
+
+let containerDiv = document.getElementById("container");
+
+for (let i = 0; i < zoznam.length; i++) {
+
+    containerDiv.innerHTML += "<p>" + zoznam[i].priklad + " = " + zoznam[i].vysledok + "</p>"
+
+}
+
+
+function vypocitaj() {
+
+    var num1 = document.getElementById('number1').value;
+    var num2 = document.getElementById('number2').value;
+    var operator = document.getElementById('operations').value;
+
+    switch (operator) {
+            // if(operator == "+")
+        case '+':
+            vysledok = parseInt(num1) + parseInt(num2);
+            console.log(num1 + ' + ' + num2 + ' = ' + vysledok);
+            document.getElementById('result').value = vysledok;
+            break;
+            // if(operator == "-")
+        case '-':
+            vysledok = parseInt(num1) - parseInt(num2);
+            console.log(num1 + ' - ' + num2 + ' = ' + vysledok);
+            document.getElementById('result').value = vysledok;
+            break;
+            // if(operator == "*")
+        case '*':
+            vysledok = parseInt(num1) * parseInt(num2);
+            console.log(num1 + ' * ' + num2 + ' = ' + vysledok);
+            document.getElementById('result').value = vysledok;
+            break;
+            // if(operator == "/")
+        case '/':
+            vysledok = parseInt(num1) / parseInt(num2);
+            console.log(`${num1} / ${num2} = ${vysledok}`);
+            document.getElementById('result').value = vysledok;
+            break;
+
+        default:
+            console.log('Invalid operation');
+
+    }
+
+    // auto vytvorime ako objekt a budeme pridavať hodnoty do zoznamu
+    let prikladInput = num1 + " " + operator + " " + num2;
+    let vysledokOutput = vysledok
+    zoznam.push({priklad: prikladInput, vysledok: vysledokOutput});
+
+    if (zoznam.length > 5) {
+        zoznam.shift();
+        // zoznam.splice(0, 1);
+    }
+
+    if (containerDiv.childElementCount > 4) {
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+
+    containerDiv.innerHTML += "<p>" + num1 + " " + operator + " " + num2 + " = " + vysledokOutput + "</p>"
+
+    // zapisanie zoznamu do local Storage 
+    localStorage.setItem("zoznam", JSON.stringify(zoznam));
+
+    console.log(zoznam);
+}
